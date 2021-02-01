4989d4860128257c1ceddc35aee648850e31915d
kvokka <kvokka@yahoo.com>
2021-02-01 03:15:24 +0000

    Fix CI

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in rubocop-replaypoker.gemspec
 gemspec
 
-gem "rake", "~> 12.0"
+gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
-gem 'rspec'
--- spec/rubocop/replaypoker_spec.rb
+++ spec/rubocop/replaypoker_spec.rb
@@ -1,5 +1,5 @@
-RSpec.describe Rubocop::Replaypoker do
+RSpec.describe RuboCop::Replaypoker do
   it "has a version number" do
-    expect(Rubocop::Replaypoker::VERSION).not_to be nil
+    expect(RuboCop::Replaypoker::VERSION).not_to be nil
   end
 end
